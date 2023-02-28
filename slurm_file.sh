6acca9124a33c636a931bff3ad8a1ef727695229
Nitay Alon <nitay.alon@gmail.com>
2023-02-28 14:42:13 +0100

    merge

--- slurm_file.sh
+++ slurm_file.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -l
-#SBATCH -o ./%A_%a_tjob.out
-#SBATCH -e ./%A_%a_tjob.err
+#SBATCH -o ./slurm_logs/%x_%j_tjob.out
+#SBATCH -e ./slurm_logs/%x_%j_tjob.err
 # Initial working directory:
 #SBATCH -D ./
-# Job Name:
-#SBATCH -J dom_m1_agent_dom_zero_subject
 #
 # Queue (Partition):
 #SBATCH --partition=compute # nyx partitions: compute, highmem, gpu
@@ -21,7 +19,6 @@
 #
 # Wall clock limit:
 #SBATCH --time=1-12:30
-
 omp_threads=$SLURM_CPUS_PER_TASK
 export OMP_NUM_THREADS=$omp_threads
 
@@ -32,9 +29,9 @@ export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
 export CONTAINER_PATH=/ptmp/containers/pytorch_1.10.0-cuda.11.3_latest-2021-12-02-ec95d31ea677.sif
 
 ENV=basic_task
-SOFTMAX_TEMP=0.05
+SOFTMAX_TEMP=0.5
 AGENT_TOM=DoM0
-SUBJECT_TOM=DoM-1
+SUBJECT_TOM=DoM0
 
 echo "Simulating with seed $SLURM_ARRAY_TASK_ID"
 time singularity exec ${CONTAINER_PATH} python main.py  --environment $ENV --seed $SLURM_ARRAY_TASK_ID --softmax_temp $SOFTMAX_TEMP --agent_tom $AGENT_TOM --subject_tom $SUBJECT_TOM
