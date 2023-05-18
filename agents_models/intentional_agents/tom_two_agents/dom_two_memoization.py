5dc224826c898479fac41425f0b0afb09b33f2a9
Nitay Alon <nitalon@cs.huji.ac.il>
2023-05-18 08:26:28 +0300

    Fix ToM(2) bug on NyX

--- agents_models/intentional_agents/tom_two_agents/dom_two_memoization.py
+++ agents_models/intentional_agents/tom_two_agents/dom_two_memoization.py
@@ -5,7 +5,7 @@ class DoMTwoMemoization(DoMOneMemoization):
     def __init__(self, path_to_memoization_dir, softmax_temp=0.1):
         self.softmax_temp = softmax_temp
         self.config = get_config()
-        self.target_table_name = f'DoM_1_memoization_data_softmax_temp_{self.softmax_temp}_seed_{self.config.seed}.csv'
+        self.target_table_name = f'DoM_1_memoization_data_softmax_temp_{self.softmax_temp}.csv'
         self.path_to_dir = path_to_memoization_dir
         self.path_to_table = os.path.join(self.path_to_dir, self.target_table_name)
         super().__init__(path_to_memoization_dir)
