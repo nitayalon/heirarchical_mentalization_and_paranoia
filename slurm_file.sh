1d398b62cfbdec2cb4a22aa61f0cb64b5478be59
Nitay Alon <nitay.alon@gmail.com>
2023-05-28 13:21:57 +0200

    pull threshold bug fix

--- slurm_file.sh
+++ slurm_file.sh
@@ -35,10 +35,10 @@ module load singularity
 export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
 export CONTAINER_PATH=/ptmp/containers/pytorch_1.10.0-cuda.11.3_latest-2021-12-02-ec95d31ea677.sif
 
-ENV=x_ipomdp
+ENV=first_task
 SOFTMAX_TEMP=0.1
 RECEIVER_TOM=DoM0
-SENDER_TOM=DoM-1
+SENDER_TOM=DoM1
 
 echo "Simulating with seed $SLURM_ARRAY_TASK_ID"
 time singularity exec ${CONTAINER_PATH} python main.py  --environment $ENV --seed $SLURM_ARRAY_TASK_ID --softmax_temp $SOFTMAX_TEMP --sender_tom $SENDER_TOM --receiver_tom  $RECEIVER_TOM 
