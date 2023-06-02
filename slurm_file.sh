a13c109a50227bbf2a03e7ee2b07ba200e7fb827
Nitay Alon <nitay.alon@gmail.com>
2023-06-02 08:24:21 +0200

    rereun DoM(1) without memoization

--- slurm_file.sh
+++ slurm_file.sh
@@ -38,7 +38,7 @@ export CONTAINER_PATH=/ptmp/containers/pytorch_1.10.0-cuda.11.3_latest-2021-12-0
 ENV=x_ipomdp
 SOFTMAX_TEMP=0.1
 RECEIVER_TOM=DoM0
-SENDER_TOM=DoM1
+SENDER_TOM=DoM-1
 
 echo "Simulating with seed $SLURM_ARRAY_TASK_ID"
 time singularity exec ${CONTAINER_PATH} python main.py  --environment $ENV --seed $SLURM_ARRAY_TASK_ID --softmax_temp $SOFTMAX_TEMP --sender_tom $SENDER_TOM --receiver_tom  $RECEIVER_TOM 
