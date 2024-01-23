97d590f01b5cfec099e5d392a71c13693278cfb4
Nitay Alon <nitalon@cs.huji.ac.il>
2024-01-23 13:31:15 +0200

    adding random row player

--- zero_sum_game/dom_m1_agent.py
+++ zero_sum_game/dom_m1_agent.py
@@ -1,21 +1,62 @@
 import numpy as np
+import argparse
+
 
 class DoMM1Player:
 
-  def __init__(self, softmax_temperature:float, discount_factor:float) -> None:
-    self.softmax_temperature = softmax_temperature
-    self.discount_factor = discount_factor
+    def __init__(self, softmax_temperature: float, discount_factor: float) -> None:
+        self.softmax_temperature = softmax_temperature
+        self.discount_factor = discount_factor
 
-  def act(self, prior_beliefs, payout_matrix, iteration):
-    q_values = dom_m1_utility(payout_matrix)
-    return softmax_transformation(q_values, self.softmax_temperature)
-      
+    def act(self, prior_beliefs, payout_matrix, iteration):
+        q_values = dom_m1_utility(payout_matrix)
+        return softmax_transformation(q_values, self.softmax_temperature)
+
+
+class RandomPlayer:
+
+    def __init__(self, softmax_temperature: float, discount_factor: float) -> None:
+        self.softmax_temperature = softmax_temperature
+        self.discount_factor = discount_factor
+
+    def act(self, prior_beliefs, payout_matrix, iteration):
+        q_values = np.repeat(1, 2)
+        return softmax_transformation(q_values, self.softmax_temperature)
+
+
 def softmax_transformation(q_values: np.array, temperature=0.01) -> np.array:
     softmax_transformation = np.exp(q_values / temperature)
     return softmax_transformation / np.sum(softmax_transformation)
 
-def dom_m1_utility(game:np.array):
-  return np.matmul(game,np.array([1/2, 1/2, 1/2]))
 
-game_1 = np.array([[4,-1,2],[4,-1,-2]])
-game_2 = np.array([[-1,4,-2],[-1,4,2]])
+def dom_m1_utility(game: np.array):
+    return np.matmul(game, np.array([1 / 2, 1 / 2, 1 / 2]))
+
+
+game_1 = np.array([[4, 0, 2], [4, 0, -2]])
+game_2 = np.array([[0, 4, -2], [0, 4, 2]])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Cognitive hierarchy task')
+    parser.add_argument('--payout_matrix', type=str, default='G1', metavar='N',
+                        help='payout matrix (default: G1)')
+    parser.add_argument('--duration', type=int, default='12', metavar='N',
+                        help='task duration (default: 12)')
+    parser.add_argument('--seed', type=int, default='6431', metavar='N',
+                        help='set simulation seed (default: 6431)')
+    parser.add_argument('--softmax_temp', type=float, default='0.05', metavar='N',
+                        help='set softmax temp (default: 0.05)')
+    parser.add_argument('--save_results', type=str, default='True', metavar='N',
+                        help='save simulation results (default: True)')
+    args = parser.parse_args()
+    duration = args.duration
+    seed = args.seed
+    payout_matrix_name = args.payout_matrix
+    payout_game = game_1 if payout_matrix_name == 'G1' else game_2
+    initial_beliefs = np.repeat(1 / 2, 2)
+    softmax_temp = args.softmax_temp
+    save_results = args.save_results
+    dom_m1_agent = DoMM1Player(softmax_temp, 0.99)
+    random_agent = RandomPlayer(softmax_temp, 0.99)
+    print(dom_m1_agent.act(None, payout_game, 0))
+    print(random_agent.act(None, payout_game, 0))
--- zero_sum_game/dom_zero_agent.py
+++ zero_sum_game/dom_zero_agent.py
@@ -7,26 +7,23 @@ class DoMZeroPlayer:
         self.softmax_temperature = softmax_temperature
         self.discount_factor = discount_factor
     
-    def irl(self, prior:np.array, observation:int, iteration: int) -> np.array:
-        p_0 = 1/2
+    def irl(self, prior:np.array, observation:int, iteration: int) -> np.array:        
         p_1 = softmax_transformation(dom_m1_utility(game_1))[observation]
         p_2 = softmax_transformation(dom_m1_utility(game_2))[observation]
-        unnormalized_posterior = prior * np.array([p_0, p_1, p_2])
+        unnormalized_posterior = prior * np.array([p_1, p_2])
         normalized_posterior = unnormalized_posterior / np.sum(unnormalized_posterior) 
         return(normalized_posterior)
     
-    def dom_zero_utility_subroutine(self, action:int , policies_matrix:np.array):
-        random_payoff = 1/2 * np.matmul(game_1[:,action],policies_matrix[0,:]) + 1/2 * np.matmul(game_2[:,action],policies_matrix[0,:])
-        p_1_payoff = np.matmul(game_1[:,action], policies_matrix[1,:])
-        p_2_payoff = np.matmul(game_2[:,action], policies_matrix[2,:])
-        return random_payoff,p_1_payoff,p_2_payoff
+    def dom_zero_utility_subroutine(self, action:int , policies_matrix:np.array):        
+        p_1_payoff = np.matmul(game_1[:,action], policies_matrix[0,:])
+        p_2_payoff = np.matmul(game_2[:,action], policies_matrix[1,:])
+        return p_1_payoff,p_2_payoff
 
     def act(self,belief):
-        # Optimal policies per type
-        pi_0 = np.array([1/2, 1/2])
+        # Optimal policies per type        
         pi_1 = softmax_transformation(dom_m1_utility(game_1))
         pi_2 = softmax_transformation(dom_m1_utility(game_2))
-        policies_matrix = np.array([pi_0, pi_1, pi_2])
+        policies_matrix = np.array([pi_1, pi_2])
         u_1 = -1 * np.matmul(belief, self.dom_zero_utility_subroutine(0, policies_matrix))
         u_2 = -1 * np.matmul(belief, self.dom_zero_utility_subroutine(1, policies_matrix))
         u_3 = -1 * np.matmul(belief, self.dom_zero_utility_subroutine(2, policies_matrix))
