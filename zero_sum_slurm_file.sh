e90611c98a3c8ce3e07a0624085e5c324f7135fc
Nitay Alon <nitay.alon@gmail.com>
2024-01-26 10:12:54 +0100

    simulate 0s game

--- zero_sum_slurm_file.sh
+++ zero_sum_slurm_file.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+_#!/bin/bash -l
 #SBATCH -o ./slurm_logs/%x_%j_tjob.out
 #SBATCH -e ./slurm_logs/%x_%j_tjob.err
 # Initial working directory:
@@ -25,9 +25,12 @@ module load singularity
 export SINGULARITY_BIND="/run,/ptmp,/scratch,/tmp,/opt/ohpc,${HOME}"
 export CONTAINER_PATH=/ptmp/containers/pytorch_1.10.0-cuda.11.3_latest-2021-12-02-ec95d31ea677.sif
 
-GAME=G1
+GAME=G_1
 SOFTMAX_TEMP=0.1
 DURATION=12
+Aleph_Ipomdp=False
+DELTA = 1.1
 
 echo "Simulating with seed $SLURM_ARRAY_TASK_ID"
-time singularity exec ${CONTAINER_PATH} python zero_sum_game/zero_sum_game_task.py  --payout_matrix $GAME --seed $SLURM_ARRAY_TASK_ID --softmax_temp $SOFTMAX_TEMP --duration $DURATION
+time singularity exec ${CONTAINER_PATH} python zero_sum_game/zero_sum_game_task.py  --payout_matrix $GAME --seed $SLURM_ARRAY_TASK_ID --softmax_temp $SOFTMAX_TEMP --duration $DURATION -aleph_ipomdp $Aleph_Ipomdp --strong_typicality_delta $DELTA 
+
