3793bb16b2ce7f63674733bc57e0f567573a55c4
Nitay Alon <nitay.alon@gmail.com>
2024-01-26 13:25:39 +0100

    pull softmax explosion bug

--- zero_sum_slurm_file.sh
+++ zero_sum_slurm_file.sh
@@ -28,8 +28,8 @@ export CONTAINER_PATH=/ptmp/containers/pytorch_1.10.0-cuda.11.3_latest-2021-12-0
 GAME=G_1
 SOFTMAX_TEMP=0.1
 DURATION=12
-Aleph_Ipomdp=True
-DELTA = 1.1
+Aleph_Ipomdp=False
+DELTA=1.1
 
 echo "Simulating with seed $SLURM_ARRAY_TASK_ID"
 time singularity exec ${CONTAINER_PATH} python zero_sum_game/zero_sum_game_task.py  --payout_matrix $GAME --seed $SLURM_ARRAY_TASK_ID --softmax_temp $SOFTMAX_TEMP --duration $DURATION --aleph_ipomdp $Aleph_Ipomdp --strong_typicality_delta $DELTA 
