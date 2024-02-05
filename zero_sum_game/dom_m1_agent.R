e2ca203de5fa54d26ab05aa15cd0b61cc6820d30
Nitay Alon <nitalon@cs.huji.ac.il>
2024-02-05 20:53:08 +0200

    Remove zero sum game

--- zero_sum_game/dom_m1_agent.R
+++
@@ -1,4 +0,0 @@
-dom_m1_utility <- function(game)
-{
-  return(game %*% c(1/2, 1/2, 1/2))
-}
--- zero_sum_game/general_functions.R
+++
@@ -1,5 +0,0 @@
-softmax <- function(q_values, temp=0.01)
-{
-  softmax_transformation <- exp(q_values / temp)
-  return(softmax_transformation / sum(softmax_transformation))
-}
